1fdeb412c0ab9fc2c724aea008871ba5fb81427c
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-03-27 23:44:17 +0200

    Add: SetData Api

--- src/mx_led_display.c
+++ src/mx_led_display.c
@@ -12,6 +12,9 @@
 #include "mx_led_display.h"
 
 /******************************************************************************
+ * definitions
+ ******************************************************************************/
+/******************************************************************************
  * Module variables definitions
  ******************************************************************************/
 /**
@@ -23,6 +26,19 @@ static uint8_t gEnChannelsOrder[MX_LED_DISPLAY_MAX][MX_LED_DISPLAY_MAX_SEG];
  * @brief The configuration pointer.
  */
 static const MxLedDisplayConfig_t* gConfig;
+
+static uint8_t SevSegTable[] = {
+  0x3F, //0
+  0x06, //1
+  0x5B, //2
+  0x4F, //3
+  0x66, //4
+  0x6D, //5
+  0x7D, //6
+  0x07, //7
+  0x7F, //8
+  0x6F, //9
+};
 /******************************************************************************
  * Function definitions
  ******************************************************************************/
@@ -58,6 +74,9 @@ MxLedDisplay_Update(void)
 
   for(Display = 0; Display < MX_LED_DISPLAY_MAX; Display++)
     {
+      //For Anode 7-segment
+      //TODO: implement for cathode
+
       //Disable the last channel
       PrevChannel = gEnChannelsOrder[Display][SevSeg];
       Dio_ChannelWrite(gConfig[Display].EnableChannels[PrevChannel], DIO_STATE_LOW);
@@ -68,6 +87,43 @@ MxLedDisplay_Update(void)
 
       //Enable the next channel
       Dio_ChannelWrite(gConfig[Display].EnableChannels[NextChannel], DIO_STATE_HIGH);
+    }
+}
+
+extern void 
+MxLedDisplay_SetData(MxLedDisplay_t Display, uint8_t SevSegNum, uint8_t Data)
+{
+
+  if(!(
+      Display < MX_LED_DISPLAY_MAX &&
+      SevSegNum > 0 && 
+      SevSegNum <= gConfig[Display].EnableChannelsSize &&
+      Data <= 9
+      ))
+    {
+      //TODO: handle this error
+      return;
+    }
+
+  uint8_t SevSegOutput;
+  uint8_t SevSegLed;
+  DioChannel_t SevSegLedChannel;
+  
+  SevSegOutput = SevSegTable[Data];
+  for(SevSegLed = 0; SevSegLed < 7; SevSegLed++)
+    {
+      //For Anode 7-segment
+      //TODO: implement for cathode
+
+      SevSegLedChannel = gConfig[Display].DataChannels[SevSegLed];
+      if(SevSegOutput & (1 << SevSegLed))
+        {
+          Dio_ChannelWrite(SevSegLedChannel, DIO_STATE_LOW);
+        }
+      else
+        {
+          Dio_ChannelWrite(SevSegLedChannel, DIO_STATE_HIGH);
+        }
     }
 }
 /*****************************End of File ************************************/
